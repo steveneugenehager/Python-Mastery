f287d672ef3066d3dd1159870723f369d1f6a98d
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 12:43:30 -0600

    intro to methods

---
+++ methods/string_methods/example_capitalization_string_methods.py
@@ -0,0 +1,8 @@
+name = 'sTeVe hAGeR'
+
+print("\t\tOriginal\t->" , name)
+print("\t\tUpper\t\t->" , name.upper())
+print("\t\tLower\t\t->" , name.lower())
+print("\t\tCapitalize\t->" , name.capitalize())
+print("\t\tTitle\t\t->" , name.title())
+print("\tOriginal is unchanged\t->" , name)
