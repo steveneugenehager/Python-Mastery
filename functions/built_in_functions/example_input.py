88f1da3e5004c2b9fc46ede69921aaa6f7af61b8
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 10:57:47 -0600

    Update example_input.py
    v2. Added display of type(input()). Test the script to show numbers entered are strings.

--- functions/built_in_functions/example_input.py
+++ functions/built_in_functions/example_input.py
@@ -1,7 +1,11 @@
 # Simple example(s) of the built-in "input()" function.
 # The output of input() is always a str.
 # Numbers returned as strings would have to be cast before using arithmetic on them.
-# Casts would need to be tested for conversion/type errors. 
+# Casts would need to be tested for conversion/type errors.
+# 
+# 2025-11-12 v2. Added display of type(input()). Test the script to show numbers entered are strings.
+ 
 
 user_input = input("Enter a string: ")
 print("INFO|The user entered the string '" , user_input , "'.",sep="")
+print("INFO|The data type of the return value of the input() function is:" , type(user_input))
