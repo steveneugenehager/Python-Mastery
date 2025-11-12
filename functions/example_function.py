70d35e3418083dfd5fe2eafd8a72092f97aa3e60
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 10:42:15 -0600

    Update example_function.py

--- functions/example_function.py
+++ functions/example_function.py
@@ -8,7 +8,8 @@
 # It is a best practice to invoke main() conditionally to support the cases where the
 #   python script is tested standalone and also from an import statement in another script.
 #
-# 2025-11-12 Created. To Do:  How should None be handled?  This occurs when the argument is invalid.
+# 2025-11-12 v1.  Created. To Do:  How should None be handled?  This occurs when the argument is invalid.
+# 2025-11-12 v2.  Added test cases for edge cases - None and Booleans. A list of numbers.
 #
 # Purpose: Demonstrate a simple function definition and execute it with a few arguments of differing types.
 
@@ -19,7 +20,7 @@ def main():
     print("INFO|Running " , __name__)
     # test integers, decimals, floats, imaginary.
     # test invalid data.
-    numbers = [ 0 , 9 , 3.14 , 3e20, -7 , 3+7j , 'cat' ]
+    numbers = [ 0 , 9 , 3.14 , 3e20, -7 , 3+7j , 'cat' , None , True , False , [1,2] ]
     for n in numbers:
         print(n , ' squared is ', square(n))
 
@@ -29,9 +30,9 @@ def main():
 def square(n):
     print("INFO|Running " , inspect.currentframe().f_code.co_name)
     try:
-        return n ** 2 
+        return (n ** 2)
     except TypeError:
-        print("ERROR|Invalid data type.  Numbers only.")
+        print("ERROR|Invalid data type.  Singleton Numbers only.")
 
 
 # This should always be used. This construct allows the flow of the program to be laid out
