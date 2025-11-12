98e1cd872f933cdaa3eb50e7c26d245a95510b83
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 11:52:42 -0600

    Create variables.py

---
+++ basics/variables.py
@@ -0,0 +1,27 @@
+#In Python, a variable is a name that refers to a value stored in memory. 
+# It is used to store data such as numbers, text, lists, or other objects 
+# so you can easily access and manipulate that data throughout your program.
+#
+# Variables are set using the assingment operator, '='.
+# Variables names 
+# - cannot start with a number
+# - must start with a letter or underscore
+# - can contain numbers, letters, or the underscore.
+# - capitalization matters
+
+# The preferred style for variable (and function) names in Python is lowercase with words separated by underscores 
+# (known as snake_case). This convention is part of Python's PEP 8 style guide.
+my_variable_name = 'a' # Preferred Convention for Python
+
+# Names with double underscores are reserved for special methods and attributes in Python.
+
+# for Constants:  Use uppercase with underscores.
+#   Example: 
+MAX_SIZE = 80
+
+# Lower camel case: The first letter is lowercase, and subsequent words start with uppercase.
+myVariableName  = 0  # Not the preferred convention...
+
+
+
+# 2025-11-12 v1. Created.
