053b13f7ee9901d4a33d2ebc7a05903eaf7507c6
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 10:30:48 -0600

    Create example_function.py

---
+++ functions/example_function.py
@@ -0,0 +1,42 @@
+# A function is defined using the def keyword.
+# A function definition includes the parameters (zero or more) the function accepts.
+# When a function is called from the callee the parameters are referred to as arguments.
+# Functions must be defined before used.
+# It is recommended to include a main() function first holding the most important flow of
+#   the solution that is then followed by the other subsidiary functions.
+# It is necessary to invoke main().
+# It is a best practice to invoke main() conditionally to support the cases where the
+#   python script is tested standalone and also from an import statement in another script.
+#
+# 2025-11-12 Created. To Do:  How should None be handled?  This occurs when the argument is invalid.
+#
+# Purpose: Demonstrate a simple function definition and execute it with a few arguments of differing types.
+
+import inspect
+
+
+def main():
+    print("INFO|Running " , __name__)
+    # test integers, decimals, floats, imaginary.
+    # test invalid data.
+    numbers = [ 0 , 9 , 3.14 , 3e20, -7 , 3+7j , 'cat' ]
+    for n in numbers:
+        print(n , ' squared is ', square(n))
+
+
+# it is typical to name the parameters in a meaningful way.
+# n here just means any old number.
+def square(n):
+    print("INFO|Running " , inspect.currentframe().f_code.co_name)
+    try:
+        return n ** 2 
+    except TypeError:
+        print("ERROR|Invalid data type.  Numbers only.")
+
+
+# This should always be used. This construct allows the flow of the program to be laid out
+# top to bottom.
+if __name__ == "__main__":
+    main()
+
+
