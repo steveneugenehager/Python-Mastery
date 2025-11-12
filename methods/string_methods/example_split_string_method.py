8a06f7b8d195bb7b353a91d8d410ab4e343dc187
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 13:15:59 -0600

    split and concept of sequence

---
+++ methods/string_methods/example_split_string_method.py
@@ -0,0 +1,8 @@
+#Example of the split string method.new_list
+ 
+s = 'a,b,,c'
+print("Original string: '" + s + "'")
+
+# Split on commas
+new_list = s.split(sep=',')
+print("New List after split:" , new_list )
