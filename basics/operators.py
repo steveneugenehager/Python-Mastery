49360335902741114e060cfc3e5a45040b278689
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-12 12:20:59 -0600

    Create operators.py

---
+++ basics/operators.py
@@ -0,0 +1,16 @@
+
+print("string operators...")
+# example
+fn = 'john'.strip()
+ln = 'doe'.strip()
+print ("INFO|Example of string concatentation using the + operator ->" , fn + ' ' + ln )
+
+
+print("number operators...")
+print("        Addition -> 1 + 2 =" , 1+2 )
+print("     Subtraction -> 2 - 1 =" , 2-1 )
+print("  Multiplication -> 3 * 2 =" , 3*2 )
+print("        Division -> 4 / 3 =" , 4/3 )
+print("Integer Division -> 4 // 3 =" , 4//3 )
+print("          Modulo -> 7 % 3 =" , 7%3 )
+print("  Exponentiation -> 3 ** 3 =" , 3**3 )
