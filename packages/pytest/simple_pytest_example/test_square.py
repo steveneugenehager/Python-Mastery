b5636dd7d200eea79d60ab42620f9a9cfc620c20
Steve Hager <steven.eugene.hager@gmail.com>
2025-11-13 16:18:51 -0600

    Update test_square.py

--- packages/pytest/simple_pytest_example/test_square.py
+++ packages/pytest/simple_pytest_example/test_square.py
@@ -6,12 +6,16 @@ def main():
     test_square()
 
 
-def test_square():
-    assert square(2) == 4
+def test_square_zero():
     assert square(0) == 0
+
+def test_square_positive():
+    assert square(2) == 4
     assert square(8) == 64
+
+def test_square_negative():
+    assert square(-3) == 9
     assert square(-8) == 64
-    assert square(3) == 9
 
          
 if __name__ == '__main__':
